58b056b1dac710eb638005eb98cb62dec9bf9f0c
Jeff Goldschrafe <jeff@rabb.it>
2020-08-05 08:48:17 -0400

    prometheus-storage: alertmanager/pushgateway volumes

--- terraform/prometheus-storage/values.tf
+++ terraform/prometheus-storage/values.tf
@@ -1,3 +1,21 @@
 output "prometheus_namespace" {
   value = kubernetes_namespace.prometheus.metadata[0].name
 }
+
+# FIXME: Replace with resource property reference when properly supported by
+# kubernetes-alpha provider
+output "alertmanager_volume_storage_class" {
+  value = "alertmanager-data"
+}
+
+# FIXME: Replace with resource property reference when properly supported by
+# kubernetes-alpha provider
+output "prometheus_volume_storage_class" {
+  value = "prometheus-data"
+}
+
+# FIXME: Replace with resource property reference when properly supported by
+# kubernetes-alpha provider
+output "pushgateway_volume_storage_class" {
+  value = "pushgateway-data"
+}
