ea57c52e68a9180372fff2d1755b13f84628403f
Jeff Goldschrafe <jeff@holyhandgrenade.org>
2020-08-12 12:31:34 -0400

    postgresql: Consul intention for GitLab migrations

--- terraform/postgresql/consul_intentions.tf
+++ terraform/postgresql/consul_intentions.tf
@@ -2,6 +2,12 @@
 # Consul intentions
 #
 
+resource "consul_intention" "postgresql_gitlab_migrations" {
+  source_name      = "gitlab-migrations"
+  destination_name = "postgresql"
+  action           = "allow"
+}
+
 resource "consul_intention" "postgresql_gitlab_sidekiq" {
   source_name      = "gitlab-sidekiq"
   destination_name = "postgresql"
