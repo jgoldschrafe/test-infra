00c9ca69fcb5facbaa48a66519bd6d20706d1d6c
Jeff Goldschrafe <jeff@holyhandgrenade.org>
2020-08-12 11:42:11 -0400

    gitlab-storage: Volumes for Gitaly, Redis

--- terraform/gitlab-storage/volumes.tf
+++ terraform/gitlab-storage/volumes.tf
@@ -2,28 +2,55 @@
 # Data volume
 #
 
-resource "kubernetes_manifest" "gitlab_data_volume" {
+resource "kubernetes_manifest" "gitlab_gitaly_data_volume" {
   provider = kubernetes-alpha
 
   manifest = {
     kind = "PersistentVolume"
     apiVersion = "v1"
     metadata = {
-      name = "gitlab-data"
+      name = "gitlab-gitaly-data"
       namespace = "gitlab"
       labels = {
         type = "local"
       }
     }
     spec = {
-      storageClassName = "gitlab-data"
+      storageClassName = "gitlab-gitaly-data"
       capacity = {
         storage = "10Gi"
       }
       accessModes = ["ReadWriteOnce"]
       hostPath = {
-        path = "/Users/jeff/Kubernetes/Volumes/gitlab-data"
+        path = "/Users/jeff/Kubernetes/Volumes/gitlab-gitaly-data"
       }
     }
   }
 }
+
+resource "kubernetes_manifest" "gitlab_redis_data_volume" {
+  provider = kubernetes-alpha
+
+  manifest = {
+    kind = "PersistentVolume"
+    apiVersion = "v1"
+    metadata = {
+      name = "gitlab-redis-data"
+      namespace = "gitlab"
+      labels = {
+        type = "local"
+      }
+    }
+    spec = {
+      storageClassName = "gitlab-redis-data"
+      capacity = {
+        storage = "10Gi"
+      }
+      accessModes = ["ReadWriteOnce"]
+      hostPath = {
+        path = "/Users/jeff/Kubernetes/Volumes/gitlab-redis-data"
+      }
+    }
+  }
+}
+
