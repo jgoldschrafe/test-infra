a087a00e01c85177f6ffc62f8e680ce778c81995
Jeff Goldschrafe <jeff.goldschrafe@flatiron.com>
2020-11-05 17:34:09 -0500

    Remove old consul projects

--- terraform/consul-config/README.md
+++
@@ -1,8 +0,0 @@
-Consul Config
-=============
-This project is used to apply configurations to a running Consul instance.
-server.
-
-Dependencies
-============
-- `consul`
--- terraform/consul-config/intentions.tf
+++
@@ -1,9 +0,0 @@
-#
-# Default Consul intentions
-#
-
-resource "consul_intention" "default_deny" {
-  source_name      = "*"
-  destination_name = "*"
-  action           = "deny"
-}
